2466f294c0b51e86372b5020f02e7233725d8603
NobleCatt <64055643+JAZ201107@users.noreply.github.com>
2024-11-03 22:29:56 +0800

    create train file

--- Transformer-Machine-Translation/config.py
+++ Transformer-Machine-Translation/config.py
@@ -1,27 +1,30 @@
 from yacs.config import CfgNode as CF
 
 
-__C = CF()
+def get_config():
+    __C = CF()
 
-# Training
-__C.TRAINING = CF()
-__C.TRAINING.BATCH_SIZE = 8
-__C.TRAINING.NUM_EPOCHS = 20
-__C.TRAINING.LEARNING_RATE = 1e-4
-__C.TRAINING.PRELOAD = "latest"
+    # Training
+    __C.TRAINING = CF()
+    __C.TRAINING.BATCH_SIZE = 8
+    __C.TRAINING.NUM_EPOCHS = 20
+    __C.TRAINING.LEARNING_RATE = 1e-4
+    __C.TRAINING.PRELOAD = "latest"
 
-# DATA
-__C.DATA = CF()
-__C.DATA.LANG_SRC = "en"
-__C.DATA.LANG_TGT = "it"
+    # DATA
+    __C.DATA = CF()
+    __C.DATA.LANG_SRC = "en"
+    __C.DATA.LANG_TGT = "it"
+    __C.DATA.TOKENIZER_FILE = "tokenizer_{0}.json"
 
-# MODEL
-__C.MODEL = CF()
-__C.MODEL.D_MODEL = 512
+    # MODEL
+    __C.MODEL = CF()
+    __C.MODEL.D_MODEL = 512
 
+    # EXPERIMENT
+    __C.EXPERIMENT = CF()
+    __C.EXPERIMENT.NAME = "runs/tmodel"
+    __C.EXPERIMENT.MODEL_FOLDER = "weights"
+    __C.EXPERIMENT.MODEL_BASENAME = "tmodel_"
 
-# EXPERIMENT
-__C.EXPERIMENT = CF()
-__C.EXPERIMENT.NAME = "runs/tmodel"
-__C.EXPERIMENT.MODEL_FOLDER = "weights"
-__C.EXPERIMENT.MODEL_BASENAME = "tmodel_"
+    return __C
--- Transformer-Machine-Translation/train.py
+++ Transformer-Machine-Translation/train.py
@@ -1,4 +1,15 @@
 import torch
+from model import make_model
+
+from torch.utils.data import DataLoader, Dataset, random_split
+
+from dataset import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+import os
 
 from utils import get_device, mask
 
@@ -37,3 +48,165 @@ def greedy_decode(model, src, src_mask, tokenizer_src, tokenizer_tgt, max_len):
             break
 
     return decoder_input.squeeze(0)
+
+
+@torch.no_grad()
+def run_validation(
+    model,
+    validation_ds,
+    tokenizer_src,
+    tokenizer_tgt,
+    max_len,
+    print_msg,
+    global_step,
+    writer,
+    num_samples=5,
+):
+    model.eval()
+
+    count = 0
+
+    source_texts = []
+    expected = []
+    predicted = []
+
+    try:
+        # get the console window width
+        with os.popen("stty size", "r") as console:
+            _, console_width = console.read().split()
+            console_width = int(console_width)
+    except:
+        # If we can't get the console width, use 80 as default
+        console_width = 80
+
+    for batch in validation_ds:
+        count += 1
+        encoder_input = batch["encoder_input"].to(device)
+        encoder_mask = batch["encoder_mask"].to(device)
+
+        assert encoder_input.size(0) == 1, "Batch size should be 1 for validation"
+
+        model_out = greedy_decode(
+            model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len
+        )
+
+        source_text = batch["src_text"][0]
+        target_text = batch["tgt_text"][0]
+        model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+        source_texts.append(source_text)
+        expected.append(target_text)
+        predicted.append(model_out_text)
+
+        # Print the source, target and model output
+        print_msg("-" * console_width)
+        print_msg(f"{f'SOURCE: ':>12}{source_text}")
+        print_msg(f"{f'TARGET: ':>12}{target_text}")
+        print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
+
+        if count == num_samples:
+            print_msg("-" * console_width)
+            break
+
+
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item["translation"][lang]
+
+
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = config.DATA.TOKENIZER_FILE.format(lang)
+
+    if not os.path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(
+            special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2
+        )
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+
+    return tokenizer
+
+
+def get_ds(config):
+    ds_raw = load_dataset(
+        f"{config.TRAINING}",
+        f"{config.DATA.LANG_SRC}-{config.DATA.LANG_TGT}",
+        split="train",
+    )
+
+    # Build Tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config.DATA.LANG_SRC)
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config.DATA.LANG_TGT)
+
+    # Keep 90% of the data for training
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BilingualDataset(
+        train_ds_raw,
+        tokenizer_src,
+        tokenizer_tgt,
+        config["lang_src"],
+        config["lang_tgt"],
+        config["seq_len"],
+    )
+    val_ds = BilingualDataset(
+        val_ds_raw,
+        tokenizer_src,
+        tokenizer_tgt,
+        config["lang_src"],
+        config["lang_tgt"],
+        config["seq_len"],
+    )
+
+    # Find the maximum length of each sentence in the source and target sentence
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item["translation"][config["lang_src"]]).ids
+        tgt_ids = tokenizer_tgt.encode(item["translation"][config["lang_tgt"]]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f"Max length of source sentence: {max_len_src}")
+    print(f"Max length of target sentence: {max_len_tgt}")
+
+    train_dataloader = DataLoader(
+        train_ds, batch_size=config["batch_size"], shuffle=True
+    )
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+
+def get_model(
+    config,
+    vocab_src_len,
+    vocab_tgt_len,
+):
+    model = make_model(
+        vocab_src_len,
+        vocab_tgt_len,
+        d_model=config["d_model"],
+        nhead=8,
+        num_encoder_layers=6,
+        num_decoder_layers=6,
+        dim_feedforward=2048,
+        dropout=0.1,
+    )
+
+    return model
+
+
+def train_model(config):
+    pass
+
+
+if __name__ == "__main__":
+    pass
