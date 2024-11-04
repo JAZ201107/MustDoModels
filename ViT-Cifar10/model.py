f1f71ab2dda7bdb686dc7bd5f92bfe85969ee684
NobleCatt <64055643+JAZ201107@users.noreply.github.com>
2024-11-04 18:21:56 +0800

    Init ViT Model

--- ViT-Cifar10/model.py
+++ ViT-Cifar10/model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
+from utils import clones
+
 
 class ViT(nn.Module):
     def __init__(
@@ -26,10 +28,20 @@ class ViT(nn.Module):
     def forward(self, x):
         nbatchs = x.size(0)
         x = x.reshape(nbatchs, -1, self.patch_size, self.patch_size)
-
         x = self.vit(x)
 
         return F.softmax(x, dim=-1)
+
+
+class SublayerConnection(nn.Module):
+    def __init__(self, size, dropout):
+        super().__init__()
+        self.norm = nn.LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, layer):
+        x = x + self.dropout(layer(self.norm(x)))
+        return x
 
 
 class Encoder(nn.Module):
@@ -62,39 +74,100 @@ def attention(key, query, value):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(
-        self,
-    ):
+    def __init__(self, heads, d_model, dropout=0.1):
         super().__init__()
+
+        assert d_model % heads == 0
+        self.d_k = d_model // heads
+        self.heads = heads
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
 
     def forward(
         self,
+        query,
+        key,
+        value,
     ):
-        pass
+        nbatches = query.size(0)
+
+        query, key, value = [
+            lin(x).view(nbatches, -1, self.heads, self.d_k).transpose(1, 2)
+            for lin, x in zip(self.linrears, (query, key, value))
+        ]
+
+        x, self.attn = attention(query, key, value, dropout=self.dropout)
+
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.heads * self.d_k)
+
+        del query
+        del key
+        del value
+
+        return self.linears[-1](x)
 
 
 class PositionEmbedding(nn.Module):
-    def __init__(
-        self,
-    ):
+    def __init__(self, image_size, patch_size, num_channels, hidden_size):
         super().__init__()
 
-    def forward(
-        self,
-    ):
-        pass
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.num_channels = num_channels
+        self.hidden_size = hidden_size
+
+        self.num_patches = (image_size // patch_size) ** 2
+
+        self.projection = nn.Conv2d(
+            self.num_channels,
+            self.hidden_size,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+        )
+
+    def forward(self, x):
+        x = self.projection(x)
+        x = x.flatten(2).transpose(1, 2)
+
+        return x
+
+
+class Embeddings(nn.Module):
+    def __init__(self, hidden_size, hidden_dropout_prob, position_embedding):
+        super().__init__()
+
+        self.path_embeddings = position_embedding
+
+        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden_size))
+        self.position_embeddings = nn.Parameter(
+            torch.randn(1, position_embedding.num_patches + 1, hidden_size)
+        )
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+
+    def forward(self, x):
+        x = self.path_embeddings(x)
+
+        batch_size = x.size(0)
+
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.position_embeddings
+        x = self.dropout(x)
+
+        return x
 
 
 class MLP(nn.Module):
-    def __init__(
-        self,
-    ):
+    def __init__(self, d_model, d_ff, dropout=0.1):
         super().__init__()
 
-    def forward(
-        self,
-    ):
-        pass
+        self.layer = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+
+        return self.dropout(self.layer(x).relu())
 
 
 def make_vit():
